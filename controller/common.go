99d75c8b0c463868233b7889479f09362b1ae9c4
haile01 <ledinhhai1312@gmail.com>
2024-04-14 06:49:06 +0700

    WHO WE ARE

--- controller/common.go
+++ controller/common.go
@@ -33,33 +33,38 @@ type InfoData struct {
 	FirstName string
 	LastName string
 	Desc string
+	Account string
 }
 
 func info(c *gin.Context) {
 	view.Execute(c, "info", []InfoData{
 		InfoData{
-			Img: "",
+			Img: "static/images/nhu.png",
 			FirstName: "Trần Nguyễn",
 			LastName: "Huế Như",
 			Desc: "Software Engineer",
+			Account: "2power9",
 		},
 		InfoData{
-			Img: "",
+			Img: "static/images/hai.png",
 			FirstName: "Lê",
 			LastName: "Đình Hải",
 			Desc: "Software Engineer",
+			Account: "haile01",
 		},
 		InfoData{
-			Img: "https://media.discordapp.net/attachments/1221316046171017239/1228657723986546698/e91124a81138d6668f29.jpg?ex=662cd7ac&is=661a62ac&hm=99e69276a29f007445e7b86dc6fdd514815b374b530c3ab7bc0c66a86b641569&=&format=webp",
+			Img: "static/images/khoa.png",
 			FirstName: "Nguyễn Công",
 			LastName: "Anh Khoa",
 			Desc: "AI Engineer",
+			Account: "ncakhoa",
 		},
 		InfoData{
-			Img: "https://cdn.discordapp.com/attachments/1221316046611284009/1228656737813270591/IMG_20231006_095308.jpg?ex=662cd6c1&is=661a61c1&hm=88e1f55b53d76fa48ca9e3c4e04142cbace566740cbed0e2c33c10e33967bc6b&",
+			Img: "static/images/truc.png",
 			FirstName: "Huỳnh Ngô",
 			LastName: "Trung Trực",
 			Desc: "AI Engineer",
+			Account: "TrucJ",
 		},
 	})
 }
