d61161dc1adebad2c22e8332d9c0cc446eef0ca2
haile01 <ledinhhai1312@gmail.com>
2024-04-14 05:32:59 +0700

    Partial about-us; More texts to fill the empty landing page

--- controller/common.go
+++ controller/common.go
@@ -30,7 +30,8 @@ func documentation(c *gin.Context) {
 
 type InfoData struct {
 	Img string
-	Name string
+	FirstName string
+	LastName string
 	Desc string
 }
 
@@ -38,23 +39,27 @@ func info(c *gin.Context) {
 	view.Execute(c, "info", []InfoData{
 		InfoData{
 			Img: "",
-			Name: "Trần Nguyễn Huế Như",
-			Desc: "...",
+			FirstName: "Trần Nguyễn",
+			LastName: "Huế Như",
+			Desc: "Software Engineer",
 		},
 		InfoData{
 			Img: "",
-			Name: "Lê Đình Hải",
-			Desc: "...",
+			FirstName: "Lê",
+			LastName: "Đình Hải",
+			Desc: "Software Engineer",
 		},
 		InfoData{
-			Img: "",
-			Name: "Nguyễn Công Anh Khoa",
-			Desc: "...",
+			Img: "https://media.discordapp.net/attachments/1221316046171017239/1228657723986546698/e91124a81138d6668f29.jpg?ex=662cd7ac&is=661a62ac&hm=99e69276a29f007445e7b86dc6fdd514815b374b530c3ab7bc0c66a86b641569&=&format=webp",
+			FirstName: "Nguyễn Công",
+			LastName: "Anh Khoa",
+			Desc: "AI Engineer",
 		},
 		InfoData{
-			Img: "",
-			Name: "Huỳnh Ngô Trung Trực",
-			Desc: "...",
+			Img: "https://cdn.discordapp.com/attachments/1221316046611284009/1228656737813270591/IMG_20231006_095308.jpg?ex=662cd6c1&is=661a61c1&hm=88e1f55b53d76fa48ca9e3c4e04142cbace566740cbed0e2c33c10e33967bc6b&",
+			FirstName: "Huỳnh Ngô",
+			LastName: "Trung Trực",
+			Desc: "AI Engineer",
 		},
 	})
 }
